9ee4b63394914da832d46486bce7833bedfca04d
mccarthinator <mccarthy.cath@gmail.com>
2017-08-09 16:16:06 -0500

    updating database

--- bamazon.js
+++ bamazon.js
@@ -1,2 +1,28 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+host: 'localhost',
+port: 3306,
+user: 'root',
+password: 'root',
+database: 'bamazon_db'
+});
+
+connection.connect(function(err) {
+    createProduct();
+});
+
+function createProduct() {
+    var query = connection.query(
+        'insert into products set ?',
+        {
+            product_name: 'shake weight',
+            department_name: 'fitness',
+            price: 19.99,
+            stock_quantity: 999
+        },
+        function(err, res) {
+            console.log(res.affectedRows + ' product inserted');
+        }
+    )
+}
